41bf006e9daaa0489291700f98f1dfe244550839
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-15 18:07:42 +0200

    fetch request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
- 
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json")
+      .then(res => {
+        if(res.status != 200) {
+          console.log("ERORO#@$@%^");
+          return;
+        }
+
+        res.json().then(data => {
+          if(data != null) {
+            setArticles(data.data.children)
+          }
+        })
+      })
+  }, [subreddit])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <input type="text" className="input" value="webdev" />
       </header>
       <div className="articles">
-        
+
       </div>
     </div>
   );
